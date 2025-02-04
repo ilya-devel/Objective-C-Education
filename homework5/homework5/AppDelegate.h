1df6c255abae65b9c6a8a091067688c12c7b97bf
ilya.devel <ilya.devel@yandex.ru>
2025-02-04 21:59:45 +0500

    homework5 completed

---
+++ homework5/homework5/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  homework5
+//
+//  Created by Ильяс Альфатович on 03.02.2025.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
